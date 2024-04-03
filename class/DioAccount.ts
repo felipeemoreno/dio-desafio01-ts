04b2044f99f514b6b9201453000e5896a46d2f06
Felipe E. Moreno <contato@wr5solutions.com.br>
2024-04-03 18:28:33 -0300

    added: loan

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -40,7 +40,7 @@ export abstract class DioAccount {
     console.log(this.balance);
   };
 
-  private validateStatus = (): boolean => {
+  protected validateStatus = (): boolean => {
     if (this.status) {
       return this.status;
     }
