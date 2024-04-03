9a6b26b798f804af1cc1310e6a2235f8d9cc6d25
Felipe E. Moreno <contato@wr5solutions.com.br>
2024-04-03 18:19:20 -0300

    added: depoist and withdraw

--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
+peopleAccount.deposit(11);
+peopleAccount.withdraw(50);
+peopleAccount.getBalance();
